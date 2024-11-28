fe745cca83ecaf695c981e2cda0fbb50f23a9714
Viraj Kamble <kamvir80@gmail.com>
2024-11-28 03:54:20 +0000

    data-types-done

--- 03_datatypes_ecma/test.js
+++ 03_datatypes_ecma/test.js
@@ -1,1 +1,24 @@
 // Datatypes and ECMA standards
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello Viraj"); // We are using nodejs, not browser
+
+// console.log(3+3); console.log("Vkviraj"); // Not a good practice of writing code
+
+// Ecmascript documentation: https://tc39.es/ecma262/#sec-intro
+
+// Data Types
+let name = "Viraj"; // String
+let age = 29; // Number (Ranging 2 to power 52/53)
+let isLoggedIn = true; // Boolean => Either true/false
+
+// BigInt => For largely values numbers (Used rarely or website is big)
+// null => standalone value (Declared to identify the variable is empty)
+// undefined => value not assigned
+// symbol => unique
+
+// Object 
+
+// typeof
+console.log(typeof undefined); // Returns undefined
+console.log(typeof null); // Returns object
